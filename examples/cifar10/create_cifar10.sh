0b82b6a7f47895dead60e1b1738ae2a8a97b5d02
bleedingfight <bleedingfight@126.com>
2021-03-17 09:52:06 +0800

    添加cifar训练

--- examples/cifar10/create_cifar10.sh
+++ examples/cifar10/create_cifar10.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env sh
 # This script converts the cifar data into leveldb format.
 set -e
-
-EXAMPLE=examples/cifar10
-DATA=data/cifar10
+CAFFE_HOME=${HOME}/caffe-env 
+EXAMPLE=${CAFFE_HOME}/examples/cifar10
+DATA=${CAFFE_HOME}/data/cifar10
 DBTYPE=lmdb
 
 echo "Creating $DBTYPE..."
 
 rm -rf $EXAMPLE/cifar10_train_$DBTYPE $EXAMPLE/cifar10_test_$DBTYPE
 
-./build/examples/cifar10/convert_cifar_data.bin $DATA $EXAMPLE $DBTYPE
+convert_cifar_data.bin $DATA $EXAMPLE $DBTYPE
 
 echo "Computing image mean..."
 
-./build/tools/compute_image_mean -backend=$DBTYPE \
+compute_image_mean -backend=$DBTYPE \
   $EXAMPLE/cifar10_train_$DBTYPE $EXAMPLE/mean.binaryproto
 
 echo "Done."
